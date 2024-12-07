826c3da6fe51f14995c99e03f8aaa78f81bac5d2
yuchiii111 <yinqitang17@gmail.com>
2024-12-08 02:40:50 +0800

    feat: sample articles

--- src/demo/Demo.tsx
+++ src/demo/Demo.tsx
@@ -7,7 +7,10 @@ import {
   Divider,
   Progress,
   Flex,
+  Button,
+  Carousel,
 } from "antd";
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
 import THEME from "../style/theme";
 import ArtcleProcess from "./renderer";
 import Editor from "./editor";
@@ -16,6 +19,8 @@ import React, { useRef, useState } from "react";
 import { paragraphSpec } from "../modules/visualizer/types";
 import { processStageAtom } from "../globalState";
 import { useAtom } from "jotai";
+import { articles } from '../userstudy/articles/articledata';
+import { useEffect } from "react";
 
 const { Header, Content, Footer } = Layout;
 const { Title, Text } = Typography;
@@ -23,6 +28,53 @@ const { Title, Text } = Typography;
 const PublicityPage = () => {
   const [processStage, setProcessStage] = useAtom(processStageAtom);
   const [llmarticle, setLlmArticle] = useState<paragraphSpec[]>([]);
+  const [currentArticleIndex, setCurrentArticleIndex] = useState(1);
+  const carouselRef = useRef<any>(null);
+
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setCurrentArticleIndex((prevIndex) => {
+  //       return prevIndex === 6 ? 1 : prevIndex + 1;
+  //     });
+  //   }, 5000); 
+
+  //   return () => clearInterval(intervalId); 
+  // }, []);
+
+  // const nextArticle = () => {
+  //   setCurrentArticleIndex((prevIndex) => {
+  //     return prevIndex === 6 ? 1 : prevIndex + 1;
+  //   });
+  // };
+
+  // const prevArticle = () => {
+  //   setCurrentArticleIndex((prevIndex) => {
+  //     return prevIndex === 1 ? 6 : prevIndex - 1; 
+  //   });
+  // };
+
+  const renderArticleContent = (index: number) => {
+    const article = articles[index-1];
+    if (!article) return null;
+  
+    if (article.processed) {
+      return (
+        <div style={{ padding: "20px", minHeight: "200px"}}>
+          <div >
+            <p className="pre-wrap"><ArtcleProcess llmarticle={article.content}/></p>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div style={{ padding: "20px", minHeight: "200px" }}>
+        <div className="content-wrapper1">
+          <p className="pre-wrap">{article.content}</p>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <ConfigProvider theme={THEME}>
@@ -38,28 +90,104 @@ const PublicityPage = () => {
                 <DemoPage />
               </Col>
             </Row>
-            <Row gutter={[16, 16]}>
-              <Col span={12}>
-                <Text style={{ fontSize: "20px", fontWeight: "bold" }}>
-                  Input Article
-                </Text>
-                <Divider style={{ margin: "0 0 2% 0" }} />
-                <Editor
-                  changeLlmArticle={setLlmArticle}
-                />
+            <Layout dir="vertical">
+            <Text style={{ fontSize: "20px", fontWeight: "bold" }}>
+              Sample Articles
+            </Text>
+            <Text style={{ fontSize: "16px", fontStyle: "italic" }}>
+              Interactive word-scale in situ visualization
+            </Text>
+            <Divider style={{ margin: "0 0 0 0" }} />
+            <ConfigProvider
+              theme={{
+                token: {
+                  colorBgContainer:' rgba(76, 144, 226, 0.3)'
+                },
+              }}
+            >
+            
+              <Carousel
+                autoplay
+                arrows infinite={true}
+                dotPosition="top"
+                style={{ padding: "10px 0", background: "#ffffff" }}
+                prevArrow={<LeftOutlined />} nextArrow={<RightOutlined />}
+                effect="fade"
+                autoplaySpeed={5000} 
+              >
+                <div>
+                  <Row gutter={[16, 16]} className="content-wrapper1">
+                    <Col span={12} >
+                      {renderArticleContent(currentArticleIndex)}
+                    </Col>
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 6)}
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row gutter={[16, 16]} className="content-wrapper1">
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 1)}
+                    </Col>
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 7)}
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row gutter={[16, 16]} className="content-wrapper1">
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 2)}
+                    </Col>
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 8)}
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row gutter={[16, 16]} className="content-wrapper1">
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 3)}
+                    </Col>
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 9)}
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row gutter={[16, 16]} className="content-wrapper1">
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 4)}
+                    </Col>
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 10)}
+                    </Col>
+                  </Row>
+                </div>
+                <div>
+                  <Row gutter={[16, 16]} className="content-wrapper1">
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 5)}
+                    </Col>
+                    <Col span={12}>
+                      {renderArticleContent(currentArticleIndex + 11)}
+                    </Col>
+                  </Row>
+                </div>
+              </Carousel>
+            </ConfigProvider>
+            </Layout>
+            {/* <Row gutter={[16, 16]}>
+              <Col span={24}>
+                <div style={{ textAlign: "center", marginTop: "20px" }}>
+                  <Button onClick={prevArticle} style={{ marginRight: "10px" }}>
+                    Previous
+                  </Button>
+                  <Button onClick={nextArticle}>Next</Button>
+                </div>
               </Col>
-              <Col span={12}>
-                <Text style={{ fontSize: "20px", fontWeight: "bold" }}>
-                  Rendered Result
-                </Text>
-                <Divider style={{ margin: "0 0 2% 0" }} />
-                <Flex gap="middle" justify="space-around" wrap="nowrap">
-                  <Text style={{ whiteSpace: "nowrap" }}>Progress: </Text>
-                  <Progress percent={processStage * 25} />
-                </Flex>
-                <ArtcleProcess llmarticle={llmarticle} />
-              </Col>
-            </Row>
+            </Row> */}
           </Layout>
         </Content>
       </ConfigProvider>
